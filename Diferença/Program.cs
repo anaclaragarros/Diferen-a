b2f03142bbb7d031231e3ffd6e150ed18b007e67
Ana Clara <acgarros@gmail.com>
2023-10-26 14:18:20 -0300

    Adicionar arquivos de projeto.

---
+++ Diferença/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diferença
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                int c = Convert.ToInt32(Console.ReadLine());
+                int d = Convert.ToInt32(Console.ReadLine());
+                string diferenca = $"DIFERENCA = {(a * b - c * d)}";
+                Console.WriteLine(diferenca);
+
+            }
+        }
+}
